f8f691698e9688d40f97956617254ca5676927b6
Mark Teicher <mark.teicher@gmail.com>
2026-07-24 10:09:02 -0400

    Delete CommandoScreenSaver directory

--- CommandoScreenSaver/CommandoScreenSaver/Pixel.swift
+++
@@ -1,8 +0,0 @@
-import AppKit
-
-enum Pixel {
-    static func fill(_ c: CGContext, x: CGFloat, y: CGFloat, w: CGFloat, h: CGFloat, color: NSColor) {
-        c.setFillColor(color.cgColor)
-        c.fill(CGRect(x: x.rounded(), y: y.rounded(), width: w.rounded(), height: h.rounded()))
-    }
-}
